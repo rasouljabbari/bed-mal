5eee62693419f8189bb27da1aa9e608e0aec8778
rasouljabbari <rasoul_jabbari_1377@yahoo.com>
2021-06-14 10:04:14 +0430

    new url

--- src/assets/scripts/GeneralVariables.js
+++ src/assets/scripts/GeneralVariables.js
@@ -1,7 +1,3 @@
-// rgi
-export const MAIN_URL_IMAGE="http://192.168.2.114:3333";
-export const MAIN_URL="http://192.168.2.114:3333/api/";
+export const MAIN_URL_IMAGE="https://bedmal-core.aralstudio.top";
+export const MAIN_URL="https://bedmal-core.aralstudio.top/api";
 
-// rg
-// export const MAIN_URL_IMAGE="http://192.168.50.14:3333";
-// export const MAIN_URL="http://192.168.50.14:3333/api/";
